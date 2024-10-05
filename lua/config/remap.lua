8b39a85125252067551171a6642b14bd433f00d7
Harry McCarthy <mccarthy.harry@icloud.com>
2024-10-05 16:40:03 +1300

    add shortcuts for copying and pasting to and from the system clipboard

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,2 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>py', '"+y$')
+vim.keymap.set('v', '<leader>py', '"+y')
+
+vim.keymap.set('n', '<leader>pd', '"+d$')
+vim.keymap.set('v', '<leader>pd', '"+d')
+
+vim.keymap.set('n', '<leader>pp', '"+p')
